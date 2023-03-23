76415fe9481f6cc08f0c4b11f7459714f500f396
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-23 13:50:18 -0300

    sets and exercises drop database

---
+++ prisma/migrations/20230323164947_sets_and_exeercise_drop_database/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `exercises` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `sets` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "exercises";
+
+-- DropTable
+DROP TABLE "sets";
